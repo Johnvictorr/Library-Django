457de886d9482071121577b476ebcec6533c6a71
Johnvictorr <johnvictorxavierr@gmail.com>
2024-09-09 22:21:45 -0300

    creating templates and migrating models to the database

--- bookstoreProject/bookstoreApp/models.py
+++ bookstoreProject/bookstoreApp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    author = models.CharField(max_length=50)
+    publisher = models.CharField(max_length=50)
+    isbn = models.IntegerField()
+    pagesNumber = models.IntegerField()
+    title = models.CharField(max_length=50)
+    yearPublication = models.IntegerField()
+    emailPublisher = models.EmailField(max_length=254)
